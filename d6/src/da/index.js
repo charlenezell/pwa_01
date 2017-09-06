b405dfda7756f3e25acb4dfe891e65040ba729bb
charlenezell <charlenezell@gmail.com>
2017-09-06 23:57:12 +0800

    webpack -p includes include and uglify pluin

--- d6/src/da/index.js
+++ d6/src/da/index.js
@@ -3,7 +3,12 @@ require('isomorphic-fetch');
 require("babel-polyfill");
 
 export const getEditorList=(token='49054768277D6FB307BB10C154649770-n1')=>{
-  return fetch(`/hs/w/act/20170914//edit/list.json?tk=${token}`).then(res=>res.json())
+  return Promise.resolve({
+    data:{
+      datas:[{id:213,ddId:2313}]
+    }
+  })
+  // return fetch(`/hs/w/act/20170914//edit/list.json?tk=${token}`).then(res=>res.json())
 }
 export const getQQRecommendList = () => {
   return fetch('/qq/recomment/qq_lian_men/0-60-1.json').then(res => {
